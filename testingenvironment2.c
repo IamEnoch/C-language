dc1999be50b7ffd2bb81be540f815d847d03689d
Enoch Mwangi <enoch.mwangi@outlook.com>
2021-06-21 11:05:23 +0300

    Written code for swapping integer numbers

--- testingenvironment2.c
+++ testingenvironment2.c
@@ -1,19 +1,25 @@
 #include<stdio.h>
 
-int i,j;
-
-i = 1;
+void swap_func(int *a, int *b);
 
 int main()
 {
-do
-{
-    for(j=1;j<=i;j++)
-    {
-        printf("%d", j);
-    }
-    printf("\n");
-    i++;
+    int i,j;
+
+    printf("Enter the first number: ");
+    scanf("%d", &i);
+    printf("\nEnter the second number: ");
+    scanf("%d", &j);
+    printf("\nThe fist number is i = %d and the second number j = %d", i,j);
+    
+    swap_func(&i,&j);
+    printf("\nAfter the swap function is called, the first number is i = %d and the second number j = %d", i,j);
 }
-while(i<=5);
+void swap_func(int *a, int *b)
+{
+    int swap;
+
+    swap = *a;
+    *a = *b;
+    *b = swap;
 }
