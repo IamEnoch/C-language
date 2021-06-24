e6bacbc54a06cb27c7fdee40065c31ad93541c2b
Enoch Mwangi <enoch.mwangi@outlook.com>
2021-06-24 19:43:16 +0300

    Summation and finding the differnece using condtional statements

--- testingenvironment2.c
+++ testingenvironment2.c
@@ -1,25 +1,41 @@
-#include<stdio.h>
+/*Design a program with three functions: main(), add(), and diff(). 
+Function main receives two integer numbers then either of the two functions add() or diff() is called for execution. 
+Function diff is called if the first number is greater than the second and computes the 
+difference between the first and second number, 
+otherwise function add() is called and does the summation of the two numbers. The results are displayed in the main function().*/
 
-void swap_func(int *a, int *b);
+#include <stdio.h>
+
+void add(int, int);
+void diff(int, int);
 
 int main()
 {
-    int i,j;
+  int first,second;
+  printf("Enter the first integer:");
+  scanf("%d", &first);
 
-    printf("Enter the first number: ");
-    scanf("%d", &i);
-    printf("\nEnter the second number: ");
-    scanf("%d", &j);
-    printf("\nThe fist number is i = %d and the second number j = %d", i,j);
-    
-    swap_func(&i,&j);
-    printf("\nAfter the swap function is called, the first number is i = %d and the second number j = %d", i,j);
+  printf("\nEnter the second integer:");
+  scanf("%d", &second);
+
+  if(first > second)
+  {
+    diff(first, second);
+  }
+  else
+  {
+    add(first, second);
+  }
 }
-void swap_func(int *a, int *b)
+void diff(int a, int b)
 {
-    int swap;
-
-    swap = *a;
-    *a = *b;
-    *b = swap;
+  int diff;
+  diff = (a - b);
+  printf("\nThe difference of the numbers is: %d", diff);
+}
+void add(int a, int b)
+{
+  int add;
+  add = (a + b);
+  printf("\nThe summation of the numbers is: %d", add);
 }
